913a7cf06a8522d16e5bb683c846d5442e35d008
mrfloxus <arthursebastianfeitosa1102@gmail.com>
2026-09-20 10:42:17 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
+import "@/app/globals.css";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
